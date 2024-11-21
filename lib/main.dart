72b247b5828117c83f574e7dda7e64e4c263203f
zzzzseong <jung48182@gmail.com>
2024-11-21 22:17:57 +0900

    buildContext 및 life-cycle 메서드(initState, dispose) 예제 코드 추가

--- lib/main.dart
+++ lib/main.dart
@@ -1,141 +1,87 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_playground/widgets/button.dart';
-import 'package:flutter_playground/widgets/currency_card.dart';
+import 'package:flutter_playground/widgets/clicker.dart';
 
 void main() {
-  runApp(const App());
+  runApp(const Clicker());
 }
 
-class App extends StatelessWidget {
+class App extends StatefulWidget {
   const App({super.key});
+
+  @override
+  State<StatefulWidget> createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+
+  bool showTitle = true;
+
+  void toggleTitle() {
+    setState(() {
+      showTitle = !showTitle;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        backgroundColor: const Color(0xFF181818),
-        body: SingleChildScrollView(
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 30),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                const SizedBox(
-                  height: 80,
-                ),
-                const Row(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: [
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.end,
-                      children: [
-                        Text(
-                          "Hey, Jisung",
-                          style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 24,
-                              fontWeight: FontWeight.w800),
-                        ),
-                        Text(
-                          "Welcome back",
-                          style: TextStyle(color: Colors.white54, fontSize: 17),
-                        ),
-                      ],
-                    )
-                  ],
-                ),
-                const SizedBox(
-                  height: 30,
-                ),
-                const Text("Total Balance",
-                    style: TextStyle(
-                      color: Colors.white54,
-                      fontSize: 20,
-                    )),
-                const SizedBox(
-                  height: 5,
-                ),
-                const Text("\$5 194 482",
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 34,
-                      fontWeight: FontWeight.w600,
-                    )),
-                const SizedBox(
-                  height: 20,
-                ),
-                const Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Button(
-                        text: "Transfer",
-                        textColor: Colors.black,
-                        backgroundColor: Color(0xFFF1B33B),
-                    ),
-                    Button(
-                        text: "Request",
-                        textColor: Colors.white,
-                        backgroundColor: Color(0xFF1F2123),
-                    ),
-                  ],
-                ),
-                const SizedBox(
-                  height: 50,
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  crossAxisAlignment: CrossAxisAlignment.end,
-                  children: [
-                    const Text(
-                      'Wallets',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 36,
-                        fontWeight: FontWeight.w700,
-                      ),
-                    ),
-                    Text(
-                      'View All',
-                      style: TextStyle(
-                          color: Colors.white.withOpacity(0.8)
-                      ),
-                    ),
-                  ],
-                ),
-                const SizedBox(
-                  height: 20,
-                ),
-                const CurrencyCard(
-                  name: "Euro",
-                  code: "EUR",
-                  amount: "6 428",
-                  icon: Icons.euro,
-                  isInverted: false,
-                ),
-                Transform.translate(
-                  offset: const Offset(0, -20),
-                  child: const CurrencyCard(
-                    name: "Bitcoin",
-                    code: "BTC",
-                    amount: "9 785",
-                    icon: Icons.currency_bitcoin,
-                    isInverted: true,
-                  ),
-                ),
-                Transform.translate(
-                  offset: const Offset(0, -40),
-                  child: const CurrencyCard(
-                    name: "Dollar",
-                    code: "USD",
-                    amount: "428",
-                    icon: Icons.attach_money_outlined,
-                    isInverted: false,
-                  ),
-                ),
-              ],
-            ),
+      theme: ThemeData(
+        textTheme: const TextTheme(
+          titleLarge: TextStyle(
+            color: Colors.red
           ),
         ),
+      ),
+
+      home: Scaffold(
+        backgroundColor: Colors.white,
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              showTitle ? const MyLargeTitle() : const Text('nothing'),
+              IconButton(onPressed: toggleTitle, icon: const Icon(Icons.remove_red_eye)),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class MyLargeTitle extends StatefulWidget {
+  const MyLargeTitle({
+    super.key,
+  });
+
+  @override
+  State<MyLargeTitle> createState() => _MyLargeTitleState();
+}
+
+class _MyLargeTitleState extends State<MyLargeTitle> {
+
+  /* Widget 생성 시 호출 메서드 */
+  @override
+  void initState() {
+    super.initState();
+    print('initState');
+  }
+
+  /* Widget 소멸 시 호출 메서드 */
+  @override
+  void dispose() {
+    super.dispose();
+    print('dispose');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    print('build');
+    return Text(
+      'My Large Title',
+      style: TextStyle(
+        fontSize: 30,
+        color: Theme.of(context).textTheme.titleLarge?.color,
       ),
     );
   }
--- lib/widgets/clicker.dart
+++ lib/widgets/clicker.dart
@@ -4,11 +4,12 @@ class Clicker extends StatefulWidget {
   const Clicker({super.key});
 
   @override
-  State<StatefulWidget> createState() => _AppState();
+  State<StatefulWidget> createState() => _ClickerState();
 }
 
-class _AppState extends State<Clicker> {
+class _ClickerState extends State<Clicker> {
   List<int> numbers = [];
+  bool showTitle = true;
 
   void onClick() {
     setState(() {
@@ -16,30 +17,77 @@ class _AppState extends State<Clicker> {
     });
   }
 
+  void toggleTitle() {
+    setState(() {
+      showTitle = !showTitle;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(
+        textTheme: const TextTheme(
+          titleLarge: TextStyle(
+            color: Colors.red
+          ),
+        ),
+      ),
       home: Scaffold(
         backgroundColor: Colors.white,
         body: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              const Text(
-                'Click Count',
-                style: TextStyle(
-                  fontSize: 30,
-                ),
-              ),
-              for (var number in numbers) Text('$number'),
+              showTitle ? const ClickerTitle() : const Text('nothing'),
+              IconButton(onPressed: toggleTitle, icon: const Icon(Icons.remove_red_eye)),
               IconButton(
                 iconSize: 40,
                 onPressed: onClick,
                 icon: const Icon(Icons.add_box_rounded),
               ),
+              for (var number in numbers) Text('$number'),
             ],
           ),
         ),
+      ),
+    );
+  }
+}
+
+class ClickerTitle extends StatefulWidget {
+  const ClickerTitle({
+    super.key,
+  });
+
+  @override
+  State<ClickerTitle> createState() => _ClickerTitleState();
+}
+
+class _ClickerTitleState extends State<ClickerTitle> {
+
+  /* Widget 생성 시 호출 메서드 */
+  @override
+  void initState() {
+    super.initState();
+    print('initState');
+  }
+
+  /* Widget 소멸 시 호출 메서드 */
+  @override
+  void dispose() {
+    super.dispose();
+    print('dispose');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    print('build');
+    return Text(
+      'Click Count',
+      style: TextStyle(
+        fontSize: 30,
+        color: Theme.of(context).textTheme.titleLarge?.color,
       ),
     );
   }

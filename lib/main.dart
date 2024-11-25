a1348088b6dabc68fec0bd6e43360378f17a4502
zzzzseong <jung48182@gmail.com>
2024-11-25 19:57:08 +0900

    Flexible, Expanded 예제 코드 추가

--- lib/main.dart
+++ lib/main.dart
@@ -1,88 +1,31 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_playground/widgets/clicker.dart';
+import 'package:flutter_playground/screens/home_screen.dart';
 
 void main() {
-  runApp(const Clicker());
+  runApp(const App());
 }
 
-class App extends StatefulWidget {
+class App extends StatelessWidget {
   const App({super.key});
-
-  @override
-  State<StatefulWidget> createState() => _AppState();
-}
-
-class _AppState extends State<App> {
-
-  bool showTitle = true;
-
-  void toggleTitle() {
-    setState(() {
-      showTitle = !showTitle;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(
+        // colorScheme: ColorScheme.fromSwatch(
+          // backgroundColor: const Color(0xFFE7626C),
+        // ),
+        colorScheme: const ColorScheme.light(
+          surface: Color(0xFFE7626C),
+        ),
         textTheme: const TextTheme(
-          titleLarge: TextStyle(
-            color: Colors.red
+          displayLarge: TextStyle(
+              color: Color(0xFF232B55)
           ),
         ),
+        cardColor: const Color(0xFFF4EDDB)
       ),
-
-      home: Scaffold(
-        backgroundColor: Colors.white,
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              showTitle ? const MyLargeTitle() : const Text('nothing'),
-              IconButton(onPressed: toggleTitle, icon: const Icon(Icons.remove_red_eye)),
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-}
-
-class MyLargeTitle extends StatefulWidget {
-  const MyLargeTitle({
-    super.key,
-  });
-
-  @override
-  State<MyLargeTitle> createState() => _MyLargeTitleState();
-}
-
-class _MyLargeTitleState extends State<MyLargeTitle> {
-
-  /* Widget 생성 시 호출 메서드 */
-  @override
-  void initState() {
-    super.initState();
-    print('initState');
-  }
-
-  /* Widget 소멸 시 호출 메서드 */
-  @override
-  void dispose() {
-    super.dispose();
-    print('dispose');
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    print('build');
-    return Text(
-      'My Large Title',
-      style: TextStyle(
-        fontSize: 30,
-        color: Theme.of(context).textTheme.titleLarge?.color,
-      ),
+      home: const HomeScreen(),
     );
   }
 }

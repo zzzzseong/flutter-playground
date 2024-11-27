538b27aa8f8d098ef7c581a7d54ca64dcc368557
zzzzseong <jung48182@gmail.com>
2024-11-27 22:11:35 +0900

    PageRoute 예제 코드 추가

---
+++ lib/webtoon/screens/detail_screen.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+
+class DetailScreen extends StatelessWidget {
+
+  final String title, thumb, id;
+
+  const DetailScreen({
+    super.key,
+    required this.title,
+    required this.thumb,
+    required this.id
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        elevation: 3,
+        surfaceTintColor: Colors.white,
+        shadowColor: Colors.black,
+        backgroundColor: Colors.white,
+        foregroundColor: Colors.green,
+        title: Text(
+          title,
+          style: const TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.w600
+          ),
+        ),
+      ),
+      body: Column(
+        children: [
+          const SizedBox(
+            height: 50,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                  clipBehavior: Clip.hardEdge,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      boxShadow: [
+                        BoxShadow(
+                            blurRadius: 15,
+                            offset: const Offset(10, 10),
+                            color: Colors.black.withOpacity(0.5)
+                        )
+                      ]
+                  ),
+                  width: 250,
+                  child: Image.network(
+                    thumb,
+                    headers: const {"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36",},
+                  )
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/webtoon/screens/home_screen.dart
+++ lib/webtoon/screens/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_playground/webtoon/models/cartoon_model.dart';
+import 'package:flutter_playground/webtoon/widgets/cartoon_widget.dart';
 
 import '../services/api_service.dart';
 
@@ -56,38 +57,7 @@ class HomeScreen extends StatelessWidget {
       padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 10),
       itemBuilder: (context, index) {
         var cartoon = snapshot.data![index];
-        return Column(
-          children: [
-            Container(
-              clipBehavior: Clip.hardEdge,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(10),
-                boxShadow: [
-                  BoxShadow(
-                    blurRadius: 15,
-                    offset: const Offset(10, 10),
-                    color: Colors.black.withOpacity(0.5)
-                  )
-                ]
-              ),
-              width: 250,
-              child: Image.network(
-                cartoon.thumb,
-                headers: const {"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36",},
-              )
-            ),
-            const SizedBox(
-              height: 10,
-            ),
-            Text(
-              cartoon.title,
-              style: const TextStyle(
-                  fontSize: 20,
-                  fontWeight: FontWeight.w600
-              ),
-            ),
-          ]
-        );
+        return Cartoon(title: cartoon.title, thumb: cartoon.thumb, id: cartoon.id);
       },
       separatorBuilder: (context, index) {
         return const SizedBox(
